9855bf51bf5c5ac7943a3c3c7f166a6157df1d5c
Vernika Chauhan <vernikachauhan1997@gmail.com>
2023-12-29 22:15:15 +0530

    Update LaunchBrowser.java

--- src/test/java/Base/LaunchBrowser.java
+++ src/test/java/Base/LaunchBrowser.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
